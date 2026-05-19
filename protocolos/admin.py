839433078d4092dc22675a8d43690c7ee3330aed
José Milton <miltoneto4231@gmail.com>
2026-05-19 19:20:04 -0300

    adicionando medicamentos por superuser

--- protocolos/admin.py
+++ protocolos/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Protocolo, Sintoma, Categoria
+from .models import Protocolo, Sintoma, Categoria, Medicamento
 
 
 admin.site.register(Protocolo)
 admin.site.register(Sintoma)
 admin.site.register(Categoria)
+admin.site.register(Medicamento)
