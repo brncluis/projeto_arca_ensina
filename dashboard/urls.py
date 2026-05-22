3dec049d2010a449ebfa24878d9094ac4bbe42e3
José Milton <miltoneto4231@gmail.com>
2026-05-21 21:28:33 -0300

    Criando a tela de cadastro de pacientes novos

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('pacientes/', views.lista_pacientes, name='pacientes'),
     path('pacientes/<int:id>/', views.historico_paciente, name='historico_paciente'),
+    path('cadastrar/', views.cadastrar_paciente, name='cadastrar_paciente'),
 ]
